f2da0183c0c6cae152345c18ff577a730fa79c2a
Mehmet Can Şimşek <82471515+mcansimsek98@users.noreply.github.com>
2024-11-27 14:43:31 +0300

    Designed Registration UI.

---
+++ Instagram-Clone/Core/Authentication/CompleteSingUpView.swift
@@ -0,0 +1,55 @@
+//
+//  CompleteSingUpView.swift
+//  Instagram-Clone
+//
+//  Created by Mehmet Can Şimşek on 27.11.2024.
+//
+
+import SwiftUI
+
+struct CompleteSingUpView: View {
+    @Environment(\.dismiss) var dissmiss
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Spacer()
+            Text("Wellcome to instagram, \nfjasdkfkasdjf")
+                .font(.title2)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .padding(.top)
+            
+            Text("Click below to complete registration and start using instagram")
+                .font(.footnote)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 24)
+
+            Button {
+                
+            } label: {
+                Text("Complete Sign Up")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 360, height: 40)
+                    .background(Color(.systemBlue))
+                    .cornerRadius(8)
+            }
+            .padding(.vertical)
+            Spacer()
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Image(systemName: "chevron.left")
+                    .imageScale(.large)
+                    .onTapGesture {
+                        dissmiss()
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    CompleteSingUpView()
+}
